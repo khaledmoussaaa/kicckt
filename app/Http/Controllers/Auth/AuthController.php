9c3d252cae5669ab4a56edf044297b874869d56e
Khaled Moussa <khaledmoussa909@gmail.com>
2024-12-21 23:34:31 +0200

    Add recent created in me api

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
     // Get the authenticated User.
     public function me()
     {
-        return contentResponse(auth()->user()->load('media'));
+        return contentResponse(array_merge(auth()->user()->load('media')->toArray(), ['is_new' => auth_user()->wasRecentlyCreated]));
     }
 
     // Refresh a token.
