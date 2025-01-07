039df833a93479570aa6eef4a1350b70967f81bb
Khaled Moussa <khaledmoussa909@gmail.com>
2025-01-07 23:14:32 +0200

    Upload last version

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(LaratrustSeeder::class);
-        // $this->call(UserSeeder::class);
+        $this->call(LaratrustSeeder::class);
+        $this->call(UserSeeder::class);
         // User::factory()->count(10)->create();
         // Join::factory()->count(10)->create();
     }
