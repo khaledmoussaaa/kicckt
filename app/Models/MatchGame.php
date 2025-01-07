5f65de77591a9120e0f71b736a7f26edae71174c
Khaled Moussa <khaledmoussa909@gmail.com>
2025-01-08 01:25:13 +0200

    add user of man of the match in show match response

--- app/Models/MatchGame.php
+++ app/Models/MatchGame.php
@@ -51,7 +51,7 @@ class MatchGame extends BaseModel
     // players -> For each match hasMany players in match and for each player has one match
     public function manOfMatch()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'man_of_the_match');
     }
 
 
