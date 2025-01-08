7217fb8152723216b04f5695e81239d84ab19e17
Khaled Moussa <khaledmoussa909@gmail.com>
2025-01-08 03:25:58 +0200

    Fix bugs

--- app/Http/Controllers/Players/PlayerMonthController.php
+++ app/Http/Controllers/Players/PlayerMonthController.php
@@ -10,20 +10,28 @@ class PlayerMonthController extends Controller
 {
     public function playerMonths($date, $type)
     {
+        // Parse the provided date to ensure correct format
         $date = Carbon::parse($date);
+
         // Retrieve users with their player_months for the specified month
         $players = User::with('media')->withWhereHas('player_month', function ($query) use ($date) {
             $query->whereMonth('created_at', $date);
         })->get();
 
-        // Check if there are no player_months for the month
+        // If no players have player_months for the specified month, get all users
         if ($players->isEmpty()) {
-            $playerMonths = User::with(['media', 'player_month' =>  function ($query) use ($date) {
+            $playerMonths = User::with(['media', 'player_month' => function ($query) use ($date) {
                 $query->whereMonth('created_at', $date);
             }])->WhereHasRole('user')->get()->sortBy('name')->values()->flatten();
         } else {
+            // Sort players by the specified dynamic field (goals, assists, points)
             $playerMonths = $players->sortByDesc(function ($user) use ($type) {
-                return $user->player_month->sum($type);
+                // Check if the player has a player_month record for the given month
+                if ($user->player_month) {
+                    // Access the specific type field (goals, assists, points)
+                    return $user->player_month->$type;  // Access dynamically based on $type
+                }
+                return 0; // If no player_month exists, return 0 for sorting
             })->values();
         }
 
