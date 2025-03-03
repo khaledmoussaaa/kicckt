a2500b16dd16934d3710a431faec4392fec5dc05
Khaled Moussa <khaledmoussa909@gmail.com>
2025-03-04 01:53:27 +0200

    Fix bugs v1.0

--- app/Events/PlayerJoin.php
+++ app/Events/PlayerJoin.php
@@ -16,17 +16,15 @@ class PlayerJoin implements ShouldBroadcast
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
-    }
+    public function __construct() {}
 
     /**
      * Get the channels the event should broadcast on.
      *
      * @return array<int, \Illuminate\Broadcasting\Channel>
      */
-    public function broadcastOn()
+    public function broadcastOn(): Channel
     {
-        return new Channel('test');
+        return new PrivateChannel('test.3'); // Ensure unique channel per chat
     }
 }
